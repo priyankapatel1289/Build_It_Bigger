e7d4be6104e4407b26d84c54d63683714147b7d7
Priyanka Patel <priyankapatel_89@yahoo.com>
2018-06-05 13:47:54 -0500

    initial commit

---
+++ jokesLib/src/main/java/patel/priyanka/jokeslib/JokesTellerJava.java
@@ -0,0 +1,20 @@
+package patel.priyanka.jokeslib;
+
+import java.util.Random;
+
+public class JokesTellerJava {
+
+    final static String[] JOKES_ARRAY = new String[]
+            {
+                    "1. This is a test joke",
+                    "2. Another test joke",
+                    "3. Well this isn't funny anymore",
+                    "4. Are you ever gonna stop?",
+                    "5. Seriously don't care about this"
+            };
+
+    public static String moreJokes() {
+        Random random = new Random();
+        return JOKES_ARRAY[random.nextInt(JOKES_ARRAY.length)];
+    }
+}
